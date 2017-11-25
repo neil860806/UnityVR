b3bf7debcee5ae2fc39cb53ad8de13edfaf5ea8f
4419-1-2\user <neil860806@gmail.com>
2017-11-25 14:46:05 +0800

    1.update 2017/11/25

--- Assets/Scripts/ShootingGalleryController.cs
+++ Assets/Scripts/ShootingGalleryController.cs
@@ -15,6 +15,11 @@ public class ShootingGalleryController : MonoBehaviour //Class 類別
     public float gameDuration = 30f;
     public float endDelay = 1.5f;
 
+    public Collider spawnCollider;
+    public ObjectPool targetObjectPool;
+    public float spawnProbabilty = 0.7f;
+    public float spawnInterval = 1f;
+
     public bool IsPlaying //Property 屬性
     {
         private set; //只有自己能改
@@ -51,15 +56,41 @@ public class ShootingGalleryController : MonoBehaviour //Class 類別
         SessionData.Restart();
 
         float gameTimer = gameDuration;
+        float spawnTimer = 0f;
         while (gameTimer > 0f)
         {
+            if (spawnTimer <= 0f)
+            {
+                if (Random.value < spawnProbabilty)
+                {
+                    spawnTimer = spawnInterval;
+                    Spawn();
+                }
+            }
             yield return null;
             gameTimer -= Time.deltaTime;
+            spawnTimer -= Time.deltaTime;
             timeBar.fillAmount = gameTimer / gameDuration;
         }
         yield return StartCoroutine(uiController.HidePlayerUI());
         IsPlaying = false;
         
+    }
+
+    private void Spawn()
+    {
+        GameObject target = targetObjectPool.GetGameObjectFromPool();
+        target.transform.position = SpawnPosition();
+    }
+
+    private Vector3 SpawnPosition()
+    {
+        Vector3 center = spawnCollider.bounds.center;
+        Vector3 extents = spawnCollider.bounds.extents;
+        float x = Random.Range(center.x - extents.x, center.x + extents.x);
+        float y = Random.Range(center.y - extents.y, center.y + extents.y);
+        float z = Random.Range(center.z - extents.z, center.z + extents.z);
+        return new Vector3(x, y, z);
     }
 
     private IEnumerator EndPhase()
